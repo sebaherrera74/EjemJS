b94f421c075d3fd5d8ce80a9fa30e9f7cdf159a3
Sebastian Herrera <sebastianh@hotmail.com.ar>
2021-12-11 13:35:57 -0300

    Hasta ejercicio empieza con nueve

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -142,14 +142,26 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0;i<arguments.length;i++){
+    total=total*arguments[i];
+  }
+  return total;
 }
+
+
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
-}
+  var contadorElementos=0;
+ for ( var i=0;i<arreglo.length;i++){
+   contadorElementos=contadorElementos+1;
+ }
+ return contadorElementos;
+ }
 
 
 function diaDeLaSemana(numeroDeDia) {
@@ -157,16 +169,47 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+
+if(numeroDeDia===1 || numeroDeDia===7){
+  return "Es fin de semana";
+}
+else {
+  return "Es dia Laboral";
+}
+
 } 
+
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  //Una forma es sacar la cantidad de digitos del numero n
+  //y comprobar si el priemer digito es nueve
   
+  var contDigitos=0;  
+  var aux=n;
+  while (aux!=0){
+    contDigitos=contDigitos+1;
+    aux=aux/10;
+  }
+    
+  for(var i=0;i<contDigitos;i++){
+    n=n/10;
+    }
+
+
+  if (n===9){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
+  
+    
+
 
 
 function todosIguales(arreglo) {
