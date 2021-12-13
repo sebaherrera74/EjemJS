a91bdfc31ca0859802ef17228a8dfeba732a1fe7
sebaherrera74 <sebaherrera152@gmail.com>
2021-12-13 12:13:03 -0300

    Avanzo uno mas

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -125,6 +125,8 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  
+
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
