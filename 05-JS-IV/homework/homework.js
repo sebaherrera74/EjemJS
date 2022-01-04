d26f1a23ca50a23bf82deb7d32604894d18d329c
sebaherrera74 <sebaherrera152@gmail.com>
2022-01-04 12:22:17 -0300

    Signed-off-by: sebaherrera74 <sebaherrera152@gmail.com>

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -144,16 +144,11 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
-
-
-
   for(var i = 0; i < usuarios.length; i++) {
     usuarios[i].esPremium = true;
   }
   return usuarios;
   }
-
-
 
 
 
@@ -166,6 +161,14 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+var suma=0;
+
+for(var i=0;i<usuario.posts.length;i++){
+  suma=suma+usuario.posts[i].likes;
+}
+return suma;
+
+
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -178,6 +181,11 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+ 
+  producto.calcularPrecioDescuento = function() {
+    return this.precio - ( this.precio * this.porcentajeDeDescuento );
+  };
+  return producto;
 
 }
 
